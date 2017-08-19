c1500b6ab01323082a1a09f5427dcf67a2ca69bd
paulocsilvajr <pauluscave@gmail.com>
2017-08-19 13:26:54 -0300

    Adicionado menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 // import { HomePage } from '../pages/home/home';
 import { ListaComprasPage } from '../pages/lista-compras/lista-compras';
+import { PesquisaProdutoPage } from '../pages/pesquisa-produto/pesquisa-produto';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,6 +15,14 @@ export class MyApp {
   // rootPage:any = HomePage;
   rootPage: any = ListaComprasPage;
 
+  public paginas = [
+    { titulo: 'Home', componente: ListaComprasPage },
+    { titulo: 'Produtos', componente: PesquisaProdutoPage }
+  ];
+
+  // captura o compomente do tipo nav na HomePage
+  @ViewChild(Nav) public nav: Nav;
+
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -21,6 +30,17 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  abrirPagina(pagina){
+    // método para abrir um item no menu a partir de atributo páginas[] depois do (click)
+    // this.nav.push(pagina.componente);
+
+    if(pagina.titulo == 'Home'){
+      this.nav.setRoot(ListaComprasPage);
+    }else{
+      this.nav.push(pagina.componente);
+    }
   }
 }
 
